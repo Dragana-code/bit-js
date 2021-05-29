fb0076651fa8771c8351a4a94b22bdc454f7f583
Dragana-code <gagaposao40@gmail.com>
2021-05-29 14:03:31 +0200

    commit

--- bit-js/index.js
+++ bit-js/index.js
@@ -1,4 +1,29 @@
-function printVendor() {
+function printWindowInfo() {
     console.log(window.navigator.vendor);
+    console.log(window.navigator.platform);
+    console.log(window.navigator.appVersion);
 }
-printVendor();
+printWindowInfo();
+
+function isOnline() {
+    if (window.navigator.isOnline) {
+        console.log('Online');
+    } else {
+        console.log('Offline');
+    }
+}
+
+isOnline();
+
+function windowScreen() {
+    console.log(window.screen.width);
+    console.log(window.screen.height);
+}
+windowScreen();
+
+function windowLocation() {
+    console.log(window.location.href);
+    console.log(window.location.hostname);
+    console.log(window.location.protocol);
+}
+windowLocation();
